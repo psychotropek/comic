99c04bb58e551657036c65650ae1290ca8a2e1bb
Eileen Koven <ekoven@outlook.com>
2022-09-24 19:08:52 -0700

    mareep script now working

--- mareep.py
+++ mareep.py
@@ -13,11 +13,12 @@ class Image:
 
 def get_file_paths_from_folder(folder_path):
     list_of_files = []
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            [base,ext] = os.path.splitext(file)
-            if ext in ['.jpg','.png','.gif']:
-                list_of_files.append(os.path.join(root,file))
+    with os.scandir(folder_path) as it:
+        for file in it:
+            if not file.name.startswith('.') and file.is_file():
+                [base,ext] = os.path.splitext(file)
+                if ext in ['.jpg','.png','.gif']:
+                    list_of_files.append(os.path.join(folder_path,file))               
     return list_of_files
 
 def convert_image(image: Image, format: str = "RGB") -> Image:
@@ -33,8 +34,6 @@ def create_image_list(file_paths: List[str]) -> List[Image]:
     return images
 
 def convert_images_to_pdf(images: List[Image], comic_path: str) -> None:
-    print(comic_path)
-    print(f'Saving {os.path.basename(comic_path)}')
     if images:
         try:
             images[0].save(comic_path, save_all=True, append_images=images[1:])
@@ -47,14 +46,17 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("comic_path", help="the base folder path")
     args = parser.parse_args()
+    [test_head, test_tail] = os.path.split(args.comic_path)
+    [_, file_prefix] = os.path.split(test_head)
 
     for root, dirs, files in os.walk(args.comic_path):
-        #if files not empty
-
         if files:
             comic_file_paths: List[str] = get_file_paths_from_folder(root) 
-            images: List[Image] = create_image_list(comic_file_paths)
-            convert_images_to_pdf(images, 'test.pdf')
+            if comic_file_paths:
+                images: List[Image] = create_image_list(comic_file_paths)
+                [head, tail] = os.path.split(root)
+                #todo - designated output folder?
+                convert_images_to_pdf(images, head + '/' + file_prefix + ' - ' + tail  + '.pdf')
 
 # If this file is the main program (if we're executing this instead of loading it)
 if __name__ == "__main__":
